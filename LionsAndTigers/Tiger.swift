e95a19e4239f5ac6729c94dd0c8e4a3ddd50699a
Marco F.A. Corzani <Marco@marcos-imac.fritz.box>
2014-09-27 01:07:14 +0200

    Kleine Änderungen

--- LionsAndTigers/Tiger.swift
+++ LionsAndTigers/Tiger.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+struct Tiger {
+
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+    
+    func chuff() {
+        println("Tiger: Chuff Chuff")
+    
+    }
+
+    func chuffNumberOfTimes(numberOfTimes:Int) {
+    
+        for var chuff = 0; chuff < numberOfTimes; ++chuff {
+            
+            // Wegen Namenskonflikt mit der Variablen obendrüber, wird die Funktion mit .self aufgerufen
+           
+            self.chuff()
+        
+        }
+    
+    }
+
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,15 +10,134 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    
+    var myTigers:[Tiger] = []
+    
+    
+    // Hier wird eine Variable mit einem INT Wert erzeugt
+    
+    var currentIndex = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
+    var myTiger = Tiger()
+        myTiger.name = "Trigger"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named:"BengalTiger.jpg")
+        
+//        myTiger.chuff()
+        myTiger.chuffNumberOfTimes(3)
+        
+        // Hier wird die erste Tigerinstanz zum Struct aus Tiger.swift hinzugefügt
+        
+        myTigers.append(myTiger)
+        
+        
+        println("ViewControler: My Tiger's name is \(myTiger.name) and its breed is \(myTiger.breed) and he is \(myTiger.age) years old and the image is \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
+        
+        //Hier wird die Variable auf den Wert 0 gesetzt
+        
+        currentIndex = 0
+        
+    var secondTiger = Tiger()
+        secondTiger.name = "Peter"
+        secondTiger.breed = "IndoChinese"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named:"IndochineseTiger.jpg")
+        
+        
+    var thirdTiger = Tiger()
+        thirdTiger.name = "Paul"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.age = 7
+        thirdTiger.image = UIImage(named:"MalayanTiger.jpg")
+        
+     
+    var fourthTiger = Tiger ()
+        fourthTiger.name = "Stefan"
+        fourthTiger.breed = "Sibirian Tiger"
+        fourthTiger.age = 6
+        fourthTiger.image = UIImage(named:"SiberianTiger.jpg")
+        
+          secondTiger.chuffNumberOfTimes(5)
+//        secondTiger.chuff()
+//        thirdTiger.chuff()
+//        fourthTiger.chuff()
+        
+        
+        
+        // Hier werden die 3 weiteren Tigerinstanzen zum Struct aus Tiger.swift hinzugefügt
+        
+        myTigers += [secondTiger, thirdTiger, fourthTiger]
+        
+      
+        
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+    
+       
+        
+        var randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        
+        while currentIndex == randomIndex {
+        
+            randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        
+        }
+        
+        currentIndex = randomIndex
+        
+        
+        let tiger = myTigers[randomIndex]
+    
+       
+        
+        UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            
+            self.myImageView.image = tiger.image
+            self.nameLabel.text = tiger.name
+            self.ageLabel.text = ("\(tiger.age) years old")
+            self.breedLabel.text = tiger.breed
+            
+            
+            }, completion: { (finished:Bool) -> () in
+    
+    }       )
+
+    }
+
 
 
 }
